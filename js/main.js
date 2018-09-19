8e30e54cd7ba10c920c13a430feb06e906719fa0
Shawn Antonucci <shawnantonucci2018@gmail.com>
2018-09-19 11:21:49 -0700

    added overflow: hidden; to fix responsiveness

--- js/main.js
+++ js/main.js
@@ -3,27 +3,27 @@ const introP = document.querySelector(".intro p");
 const nav = document.querySelectorAll("nav a");
 const motto = document.querySelector(".motto h2");
 
-// TweenMax.from(introH1, 3, {opacity:0, right: 500});
-// TweenMax.from(introP, 3, {opacity:0, left: "10%"});
-// TweenMax.from(nav, 2, {ease: Bounce.ease, opacity:0, width: 500});
-// TweenMax.from(motto, 5, {right: 1500});
+TweenMax.from(introH1, 3, {opacity:0, right: 500});
+TweenMax.from(introP, 3, {opacity:0, left: 500});
+TweenMax.from(nav, 2, {ease: Bounce.ease, opacity:0, width: 500});
+//TweenMax.from(motto, 5, {right: 1500});
 
-function myFunction(x) {
-  if (x.matches) {
-    // If media query matches
-    TweenMax.from(introH1, 3, { opacity: 0, bottom: 200 });
-    TweenMax.from(introP, 3, { opacity: 0, top: 200 });
-    TweenMax.from(nav, 2, { ease: Bounce.ease, opacity: 0});
-  } else {
-    TweenMax.from(introH1, 3, { opacity: 0, right: 500 });
-    TweenMax.from(introP, 3, { opacity: 0, left: "10%" });
-    TweenMax.from(nav, 3, { right: 1000 });
-  }
-}
+// function myFunction(x) {
+//   if (x.matches) {
+//     // If media query matches
+//     TweenMax.from(introH1, 3, { opacity: 0, bottom: 200 });
+//     TweenMax.from(introP, 3, { opacity: 0, top: 200 });
+//     TweenMax.from(nav, 2, { ease: Bounce.ease, opacity: 0});
+//   } else {
+//     TweenMax.from(introH1, 3, { opacity: 0, right: 500 });
+//     TweenMax.from(introP, 3, { opacity: 0, left: "10%" });
+//     TweenMax.from(nav, 3, { right: 1000 });
+//   }
+// }
 
-var x = window.matchMedia("(max-width: 500px)");
-myFunction(x); // Call listener function at run time
-x.addListener(myFunction); // Attach listener function on state changes
+// var x = window.matchMedia("(max-width: 500px)");
+// myFunction(x); // Call listener function at run time
+// x.addListener(myFunction); // Attach listener function on state changes
 
 // function Scroll() {
 //     const ypos = window.pageYOffset;
