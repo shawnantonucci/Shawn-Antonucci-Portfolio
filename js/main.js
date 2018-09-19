e67505ca533a13c7f7fc49ac6920d00f75115a98
Shawn Antonucci <shawnantonucci2018@gmail.com>
2018-09-18 20:34:05 -0700

    added in more animations

--- js/main.js
+++ js/main.js
@@ -1,7 +1,30 @@
 const introH1 = document.querySelector(".intro h1");
 const introP = document.querySelector(".intro p");
 const nav = document.querySelectorAll("nav a");
+const motto = document.querySelector(".motto h2");
 
 TweenMax.from(introH1, 3, {opacity:0, width: 0});
 TweenMax.from(introP, 3, {opacity:0, width: 5000});
 TweenMax.from(nav, 3, {ease: Bounce.ease, opacity:0, width: 1000});
+//TweenMax.from(motto, 5, {left: 1500});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Scroll() {
+//     const ypos = window.pageYOffset;
+//     if(ypos > 175) {
+//         TweenMax.from(motto, 5, {opacity:0});
+//     }
+// }
+
+// window.addEventListener("scroll", Scroll);
